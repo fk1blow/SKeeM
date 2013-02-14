69010e4c348649c741e0d06e48a721e7897feb4c
Dragos Tudorache <hedphug@gmail.com>
2013-02-14 12:24:35 +0200

    Refactored SKMObject to a simpler implementation
    Refactored SKMObject.extend, create to use inner package defined
    methods(extends, inherits, isObject).
    Also, refactored the extend process, which should have dealt with both
    (previous)mixins and extension params, in a similar way.

--- js/lib/skm/k/Object.js
+++ js/lib/skm/k/Object.js
@@ -6,10 +6,46 @@ define(['skm/util/ObjectUtils'], function(Utils)
 'use strict';
 
 
+// Shorthand method
+var slice = Array.prototype.slice;
+
+
+/**
+ * Extends a given object with a given
+ * array of extension objects
+ * 
+ * @param  {Object} target Destination object
+ */
+var extend = function(target) {
+  var ext = [].slice.call(arguments, 1);
+  var i, prop, extension, extLen = ext.length;
+  for (i = 0; i < extLen; i++) {
+    extension = ext[i];
+    for (prop in extension) {
+      if (extension.hasOwnProperty(prop))
+        target[prop] = extension[prop];
+    }
+  }
+}
+
+
+/**
+ * Safes test for an Object
+ * though it excludes null and Array
+ * 
+ * @param  {Mixed}  obj The object to test
+ * @return {Boolean}     
+ */
+var isObject = function(obj) {
+  return Object.prototype.toString.apply(obj) === '[object Object]';
+}
+
+
 /**
  * Inherit the prototype methods from one constructor into another.
  *
- * Taken from http://closure-library.googlecode.com/svn/docs/closure_goog_base.js.source.html
+ * @description Taken from google's closure library
+ * @link http://closure-library.googlecode.com/svn/docs/closure_goog_base.js.source.html
  */
 var inherits = function(childCtor, parentCtor) {
   /** @constructor */
@@ -34,40 +70,29 @@ var mergeObjects = function(extensions, target) {
   var item, len = extensions.length;
   for (var i = 0; i < len; i++) {
     item = extensions[i];
-    // If it's an Object, include it to this.prototype
-    if ( Utils.ObjectUtil.isObject(item) ) {
-      Utils.ObjectUtil.include(target, item);
-    }
-    // if it's not an Object, assume this is a constructor function
-    // so take its prototype as a mixin
-    else {
-      Utils.ObjectUtil.include(target, item.prototype);
-    }
+    // If it's an Object, include it to target
+    if ( isObject(item) )
+      extend(target, item);
   }
 }
 
 
 var SKMObject = function() {};
 
+
 /**
  * Creates a constructor function based its prototype
  * to an SKMObject definition
  * 
- * @param  {Object} mixins     A list of zero or more SKM/Objects
- * that representing a behaviour rather then inheritance
- * @param  {Object} properties An Object that can be used as a 
- * extension/template for this object
+ * @param  {Object} mixins     A list of zero or more Objects
+ * that represent the definition of this constructor
  * @return {Function}  function  constructor function used as a 
  * template for the new SKMObject
  */
-SKMObject.extend = function(mixins, properties) {
-  var args = [].slice.call(arguments);
-  var mixins = [].slice.call(args, 0, args.length - 1);
-  var extension = args[args.length - 1];
-  var parent = this;
-  var child = null;
+SKMObject.extend = function(mixins) {
+  var args = slice.call(arguments);
+  var parent = this, child = null;
   // Use the initialize function as a function constructor
-  // Don't check for typeof intialize - "initialize" field should always be a function
   /*if ( extension && ( 'initialize' in extension ) ) {
     child = extension.initialize;
   } else {
@@ -78,20 +103,20 @@ SKMObject.extend = function(mixins, properties) {
   child = function() {
     parent.apply(this, arguments);
   }
-  // Add the function object "static" methods
-  child.extend = parent.extend;
-  child.create = parent.create;
-  child.mixin = parent.mixin;
-  // Establish the basic proto chain
+
+  // Add static methods directly to child
+  // function constructor
+  extend(child, parent);
+
+  // Establish the base prototype chain
   inherits(child, parent);
-  // If has extension(properties), add it
-  // to the prototype of the child
-  if ( extension )
-    Utils.ObjectUtil.include(child.prototype, extension);
-  // Add the function mixins to child proto
-  if ( mixins && mixins.length )
-    mergeObjects(mixins, this.prototype);
-    // child.mixin(mixins);
+
+  // Inject every extension Object to [this.prototype]
+  if ( args.length ) {
+    args.splice(0, 0, this.prototype);
+    extend.apply(null, args);
+  }
+
   return child;
 }
 
@@ -99,20 +124,22 @@ SKMObject.extend = function(mixins, properties) {
  * [create description]
  * @return {[type]} [description]
  */
-SKMObject.create = function() {
-  var args = [].slice.call(arguments);
+SKMObject.create = function(options) {
   // Create the actual "instance"
   var instance = new this();
+
   // Check every argument if it's a SKM.Mixin or plain object
   // If it's an Object, add it to the target instance
   // If it's a Mixin, use Mixin.injectTo and add it to the instance object
-  if ( args.length ) {
-    mergeObjects(args, instance);
+  if ( arguments.length ) {
+    extend(instance, options);
   }
+
   // Try to call the initialize function
   if ( typeof instance.initialize === 'function' ) {
     instance.initialize.apply(instance, arguments);
   }
+
   return instance;
 }
 
@@ -121,7 +148,8 @@ SKMObject.create = function() {
  * or a function constructor's prototype
  */
 SKMObject.mixin = function() {
-  mergeObjects(arguments, this.prototype);
+  if ( arguments.length )
+    extend(this.prototype, arguments);
 }
 
 
