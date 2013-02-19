39dd7e9dfb600e22215ca47c11b39005bceb9ad3
Dragos Tudorache <hedphug@gmail.com>
2013-02-19 17:55:13 +0200

    Initial RTF Manager commit.

---
+++ js/lib/skm/rtf/RTFManager.js
@@ -0,0 +1,20 @@
+
+// RTF Manager implementation
+
+define(['skm/k/Object',
+  'skm/util/Logger',
+  'skm/util/Subscribable'],
+  function(SKMObject, SKMLogger, Subscribable)
+{
+'use strict';
+
+
+var Logger = SKMLogger.create();
+
+
+// handles the ConnectorManager
+// creates the connectors
+// and passes each of them to the ConnectorManager
+
+
+});
