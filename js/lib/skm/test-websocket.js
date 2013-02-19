e15ce582b8c66a6514fb4d6b913a5b5f640c4d90
Dragos Tudorache <hedphug@gmail.com>
2013-02-19 17:53:08 +0200

    update

--- js/lib/skm/test-websocket.js
+++ js/lib/skm/test-websocket.js
@@ -1,18 +1,70 @@
 
 
 
-require(['skm/net/ws/WSWrapper', 'skm/k/Object', 'skm/util/Timer'],
-  function(SKMWebsocket, SKMObject, SKMTimer)
+require(['skm/net/WSWrapper',
+				 'skm/net/XHRWrapper',
+				 'skm/k/Object',
+				 'skm/rtf/ConnectorManager',
+				 'skm/rtf/Connector'],
+  function(WSWrapper, XHRWrapper, SKMObject, RTFConnectorManager, RTFConnector)
 {
 
-// console.clear();
-
-// window.SKMTimer = SKMTimer;
-
-// window.SKMWebsocket = SKMWebsocket;
+console.clear();
 
 
-var wsUrls = [
+
+
+
+
+
+
+
+var wsConnector = RTFConnector.WS.create({
+	transport: WSWrapper.create({ url: 'ws://10.0.3.98:3000', reconnectAttempts: 2 })
+});
+// wsConnector.openConnection();
+
+var xhrUrl = 'http://10.0.3.98/testajax?subscribe=test&clientId=' + (new Date).getTime()
+var xhrConnector = RTFConnector.XHR.create({
+	transport: XHRWrapper.Wrapper.create({ url: xhrUrl })
+});
+
+var CM = RTFConnectorManager.create();
+CM.addConnector('wsconnector', wsConnector);
+// CM.addConnector('xhrconnector', xhrConnector);
+
+CM.beginUpdateUsing('wsconnector');
+
+
+
+
+
+
+
+
+
+/**
+ * TBD
+ * @type {Int}
+ */
+// clientId: null,
+
+/**
+ * TBD
+ * @type {Int}
+ */
+// batchId: null,
+
+
+
+
+
+
+
+
+return;
+
+/*var wsUrls = [
   'ws://localhost:8080/WebSocketServletTest/websk',
   'ws://10.0.3.98:3000'
 ];
@@ -27,11 +79,7 @@ ws.on('received:pong', function() {
 }).on('server:disconnected', function() {
     cl('server:disconnected')
 })
-ws.connect();
-
-// setInterval(function() {
-//   ws.send('kk')
-// }, 1000)
+ws.connect();*/
 
 
 });
