cef19f2eea3a2297ff6f1fd483e846942cd8b4f9
Dragos Tudorache <hedphug@gmail.com>
2013-03-07 17:25:12 +0200

    Added connector manager, connectors, urls

--- js/lib/skm/rtf/RTFApi.js
+++ js/lib/skm/rtf/RTFApi.js
@@ -3,7 +3,7 @@
 
 define(['skm/k/Object',
   'skm/util/Logger',
-  'skm/util/Subscribable'
+  'skm/util/Subscribable',
   'skm/net/WSWrapper',
   'skm/net/XHRWrapper',
   'skm/rtf/ConnectorManager',
@@ -16,54 +16,148 @@ define(['skm/k/Object',
 
 
 var Logger = SKMLogger.create();
-  
 
-// RTFApi implementation
-var Api = SKMObject.extend(Subscribable, {
-  _connectorManager: null,
+
+
+
+
+
+var Config = {
+  baseUrl: {
+    ws: 'ws://localhost:8080',
+    xhr: 'http://localhost:8080/testajax'
+  },
+
+  useConnectors: ['WebSocket', 'XHR']
+};
+
+
+
+
+
+
+var wsurls = [
+  'ws://localhost:8080/testws?clientId=' + (new Date().getTime()) + '&subscribe=test&batchId=1',
+  'ws://10.0.3.98:3000'
+];
+var xhrUrl = 'http://localhost:8080/testajax?subscribe=test&clientId=' + ((new Date).getTime()) + '&batchId=1';
+
+
+var connectorManager = ConnectorManager.create({
+  sequence: Config.useConnectors
+});
+
+/*connectorManager.registerConnector('WebSocket', WSConnector.create({
+  transport: WSWrapper.create({ url: wsurls[0], reconnectAttempts: 0 })
+}));
+
+connectorManager.registerConnector('XHR', XHRConnector.create({
+  transport: XHRWrapper.create({ url: xhrUrl })
+}));*/
+
+
+var subscriptionList = {
+  _subscriptions: null,
+
+  add: function(name, subscription) {
+    if ( ! this._subscriptions )
+      this._subscriptions = {};
+    this._subscriptions[name] = subscription;
+  },
+
+  remove: function(name) {
+    delete this._subscriptions[name];
+  },
+
+  get: function(name) {
+    return this._subscriptions[name];
+  },
+
+  has: function(subscription) {
+    return this._subscriptions && (subscription in this._subscriptions);
+  }
+};
+
+
+var Subscription = SKMObject.extend({
+  name: '',
 
   initialize: function() {
-    Logger.debug('%cnew RTFApi', 'color:#A2A2A2');
-    this._initConnectorManager();
+    Logger.debug('%cnew Subscription', 'color:#A2A2A2', this.name);
   },
 
-  start: function() {
-    //
+  handleUpdate: function(message) {
+    Logger.debug('%cSubscription.handleUpdate :: ', 'color:green',
+      'subscription : ' + this.name, ', message : ', message);
   },
 
-  stop: function() {
-    //
-  },
-
-  
-  /*
-    Subscriptions
-   */
-  
-
-  
-
-  subscribeTo: function(name, callback) {
-    //
-  },
-
-  unsubscribeFrom: function(name) {
-    //
-  },
-
-
-  /*
-    Private
-   */
-  
-
-  _initConnectorManager: function() {
-    this._connectorManager = ConnectorManager.create();
+  destroy: function() {
+    Logger.debug('%cSubscription.destroy', 'color:green');
   }
 });
 
 
-return Api;
+// RTFApi implementation
+var Api = {
+  startUpdates: function() {
+    Logger.debug('%cApi.start', 'color:green');
+    connectorManager.startConnectors();
+  },
+
+  stopUpdates: function() {
+    Logger.debug('%cApi.stop', 'color:green');
+  },
+
+  /**
+   * Adds a new subscription
+   *
+   * @description creates a new Subscription object
+   * and ties it to the connector's "api:update" event
+   * @param {String} name subscription name
+   */
+  addSubscription: function(name) {
+    if ( subscriptionList.has(name) ) {
+      Logger.info('Api.addSubscription :: ' + name +
+        ' subscription already registered!'); 
+    }
+    var subscription = subscriptionList[name] = Subscription.create({
+      name: name
+    });
+    // Adds the subscription to the collection
+    subscriptionList.add(name, subscription);
+    // Tie the subscription to the manager's updates
+    connectorManager.on('update', subscription.handleUpdate, subscription);
+    return subscription;
+  },
+
+  /**
+   * Removes a subscription
+   *
+   * @description destroys the instance and
+   * removes the callbacks on connector "api:update" event
+   * @param  {String} name subscription name
+   */
+  removeSubscription: function(name) {
+    var subscription = subscriptionList.get(name);
+    connectorManager.off('update', subscription.handleUpdate);
+    subscriptionList.remove(name);
+    subscription.destroy();
+  },
+
+  /**
+   * Returns a subscription
+   * @param  {String} name the name of the subscription
+   */
+  getSubscription: function(name) {
+    return subscriptionList.get(name);
+  }
+};
+
+
+return {
+  Api: Api,
+  Config: Config
+}
 
 
 });
