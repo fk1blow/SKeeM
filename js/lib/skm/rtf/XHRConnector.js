0cbdb850c992b44ca6068fe98870fc75bd9c5f95
Dragos Tudorache <hedphug@gmail.com>
2013-04-26 17:17:54 +0300

    modified connector and added delegates

--- js/lib/skm/rtf/XHRConnector.js
+++ js/lib/skm/rtf/XHRConnector.js
@@ -17,53 +17,10 @@ var ConnectorErrors = {
   INACTIVE: 'Innactive connection',
   LIST_TO_BIG: 'Confirmation Message Sent list is too big',
   READY_LIST_TO_BIG: 'Ready To send Message list is too big'
-}
+};
 
 
-var XHRConnector = BaseConnector.extend({
-  _typeName: 'XHR',
-
-  beginUpdate: function() {
-    // ensure transport type and transport url creation
-    this.ensureTransportCreated(XHRWrapper).buildTransportUrl();
-    Logger.debug('XHRConnector.beginUpdate\n', this.transport.url);
-    // because xhr is ready after being instantiated
-    this.fire('connector:ready');
-    return this;
-  },
-
-  endUpdate: function() {
-    Logger.debug('XHRConnector.endUpdate');
-    // disconnect and remove events
-    this.transport.abortRequest();
-    // this.removeTransportListeners();
-    return this;
-  },
-
-  addTransportListeners: function() {
-    this.transport
-      .on('error', this.handleError, this)
-      .on('success', this.handleReceivedMessage, this);
-    return this;
-  },
-
-
-  /*
-    Message senders
-   */
-  
-
-  sendMessage: function(message) {
-    Logger.debug('%cXHRConnector.sendMessage : ', 'color:red', message);
-    this.transport.sendMessage({ message: message });
-  },
-
-
-  /*
-    Handlers
-  */
-  
-
+var EventsDelegates = {
   /**
    * Handles a message received from server api
    *
@@ -73,8 +30,8 @@ var XHRConnector = BaseConnector.extend({
    * @param  {Object} message JSON message send by rtf server api
    */
   handleReceivedMessage: function(message) {
-    Logger.info('XHRConnector.handleReceivedMessage');
-    this.fire('api:update', message);
+    Logger.info('Connector.handleReceivedMessage');
+    this.fire('api:message', message);
   },
 
   /**
@@ -87,13 +44,86 @@ var XHRConnector = BaseConnector.extend({
    * @param  {[type]} err JSON message representing the reason
    */
   handleError: function(err) {
-    Logger.info('XHRConnector.handleError');
+    Logger.info('Connector.handleError');
     // If server triggers errors
     if ( err.status == 405 ) {
       this.fire('api:error', err.responseText);
     } else {
+      this.handleConnectingStopped();
+      // this.fire('transport:deactivated');
+    }
+  },
+
+  handleConnectingStopped: function() {
+    var that = this;
+
+    Logger.info('Connector.handleConnectingStopped');
+
+    if ( this._currentAttempt <= this.maxReconnectAttempts ) {
+      Logger.debug('Connector : will make attempt in', this.reconnectDelay, 'ms');
+      // Try to begin update and reconnect after [this.reconnectDelay]
+      setTimeout(function() {
+        Logger.debug('_____________________________________________');
+        Logger.debug('Connector : attempt #', that._currentAttempt);
+        // is reconnecting and increment current attempt
+        that._isReconnecting = true;
+        that._currentAttempt += 1;
+        // try to re-establish connection by calling [beginUpdate]
+        that.beginUpdate();
+      }, this.reconnectDelay);
+    } else {
+      Logger.debug('Connector : maxReconnectAttempts of ' 
+        + this.maxReconnectAttempts + ' reached!');
+      // has stopped reconnecting and reset current attempt
+      this._isReconnecting = false;
+      this._currentAttempt = 1;
+
+      // tell the manager the transport has been deactivated
       this.fire('transport:deactivated');
     }
+  }
+};
+
+
+var XHRConnector = BaseConnector.extend(EventsDelegates, {
+  _typeName: 'XHR',
+
+  _currentAttempt: 1,
+
+  _isReconnecting: false,
+
+  maxReconnectAttempts: 3,
+
+  reconnectDelay: 3000,
+
+  beginUpdate: function() {
+    // ensure transport type and transport url creation
+    this.ensureTransportCreated(XHRWrapper).buildTransportUrl();
+    Logger.info('Connector.beginUpdate');
+    Logger.debug('Connector : transport url :', this.transport.url);
+    // because xhr is ready after being instantiated
+    this.fire('connector:ready');
+    return this;
+  },
+
+  endUpdate: function() {
+    Logger.info('Connector.endUpdate');
+    // disconnect and remove events
+    this.transport.abortRequest();
+    // this.removeTransportListeners();
+    return this;
+  },
+
+  sendMessage: function(message) {
+    Logger.debug('%cConnector.sendMessage : ', 'color:green', message);
+    this.transport.sendMessage({ message: message });
+  },
+
+  addTransportListeners: function() {
+    this.transport
+      .on('error', this.handleError, this)
+      .on('success', this.handleReceivedMessage, this);
+    return this;
   }
 });
 
