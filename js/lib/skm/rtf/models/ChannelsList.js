f9b81ce66f27b4de6dbe1c61d14bcb5b5295dbae
Dragos Tudorache <hedphug@gmail.com>
2013-05-07 11:56:43 +0300

    Moved ChannelsList and UrlParam to separate files

---
+++ js/lib/skm/rtf/models/ChannelsList.js
@@ -0,0 +1,87 @@
+
+// RTF Connectors channelst list implementation
+
+define(['skm/k/Object',
+  'skm/util/Logger'],
+  function(SKMObject, SKMLogger) {
+'use strict';
+
+
+var ChannelsListModel = SKMObject.extend({
+  _currentList: null,
+
+  _confirmedList: null,
+  
+  addChannel: function(channel) {
+    var list = this._currentList = this._currentList || {},
+        channelItem, paramItem;
+    var channelParams = channel['params'],
+        channelName = channel['name'];
+
+    if ( channelName in list ) {
+      channelItem = list[channelName];
+    } else {
+      channelItem = list[channelName] = {};
+    }
+    // ...and add channel parameters, if any
+    for ( paramItem in channelParams ) {
+      channelItem[paramItem] = channelParams[paramItem];
+    }
+  }, 
+
+  removeChannel: function(name) {
+    var subscription = null;
+    if ( this._currentList && name in this._currentList ) {
+      delete this._currentList[name];
+    }
+    if ( this._confirmedList && name in this._confirmedList ) {
+      delete this._confirmedList[name];
+    }
+  },
+
+  // @todo move it to the api module
+  confirmChannel: function(channelName, willConfirm) {
+    var confirmed = this._confirmedList = this._confirmedList || {};
+    var list = this._currentList;
+
+    if ( channelName in list && willConfirm ) {
+      confirmed[channelName] = list[channelName];
+      delete list[channelName];
+    }
+  },
+
+  hasSubscribedAndConfirmed: function(channelObj) {
+    var list = this._confirmedList;
+    var hasSubscribed = false;
+    if ( list ) {
+      hasSubscribed = (channelObj['name'] in list);
+    }
+    return hasSubscribed;
+  },
+
+  getCurrentList: function() {
+    return this._currentList;
+  },
+
+  toStringifiedJson: function() {
+    var item, first = true, parameterized = 'subscribe:{';
+    var list = this._currentList;
+    for ( item in list ) {
+      if (!first) {
+        parameterized+= ',';
+      }
+      parameterized += item;
+      first = false;
+    }
+    parameterized += '}';
+    parameterized += 'params:' + JSON.stringify(list)
+      .replace(/\'|\"/g, '');
+    return parameterized;
+  }
+});
+
+
+return ChannelsListModel;
+
+
+});
---
+++ js/lib/skm/rtf/models/UrlParam.js
@@ -0,0 +1,88 @@
+
+// RTF Connectors Url Model implementation
+
+define(['skm/k/Object',
+  'skm/util/Logger',
+  'skm/util/Subscribable'],
+  function(SKMObject, SKMLogger, Subscribable) {
+'use strict';
+
+
+var UrlParamModel = SKMObject.extend(Subscribable, {
+  _parameterizerList: null,
+
+  getList: function() {
+    this._parameterizerList = this._parameterizerList || {};
+    return this._parameterizerList;
+  },
+
+  getByName: function(name) {
+    return this._parameterizerList[name];
+  },
+
+  toUrl: function() {
+    var list = this._parameterizerList;
+    return encodeURIComponent(JSON.stringify(list).toString());
+  },
+
+  toQueryString: function(concatStr) {
+    var i = 0, qs = '', part, segment, params = this.getList();
+    var concatWith = concatStr || '&';
+    for ( part in params ) {
+      i = 0, segment = params[part];
+      // If at first part
+      if ( qs.length < 1 ) {
+        qs += '?';
+      } else {
+        qs += concatWith;
+      }
+      // for each part, there will be a segment array
+      for ( ; i < segment.length; i++ ) {
+        if ( i > 0 )
+          qs += concatWith;
+        qs += (part + '=' + segment[i]);
+      }
+    }
+    return qs;
+  },
+
+  reset: function(name, value) {
+    if ( name in this.getList() )
+      delete this._parameterizerList[name];
+    return this;
+  },
+
+  add: function(name, value) {
+    var list = this.getList();
+    if ( list[name] ) {
+      list[name].push(value);
+    } else {
+      list[name] = [value];
+    }
+    this.fire('added');
+    return this;
+  },
+
+  remove: function(name) {
+    var list = this.getList();
+    if ( name in list )
+      delete list[name];
+    this.fire('removed');
+    return this;
+  },
+
+  alter: function(name, newValue) {
+    var param, list = this.getList();
+    if ( param = list[name] ) {
+      list[name] = [newValue];
+    }
+    this.fire('altered');
+    return this;
+  }
+});
+
+
+return UrlParamModel;
+
+
+});
