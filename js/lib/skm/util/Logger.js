6def7b30078f560bd0d0ca15cfa0fff9b429d015
Dragos Tudorache <hedphug@gmail.com>
2013-05-20 14:01:02 +0300

    cleaned the Logger and added "cl" shortcut to the file

--- js/lib/skm/util/Logger.js
+++ js/lib/skm/util/Logger.js
@@ -1,7 +1,13 @@
 
 // skm Logger module
 
-define(['skm/k/Object'], function(SKMObject) {
+define(['skm/k/Object'],
+	function(SKMObject)
+{
+'use strict';
+
+
+var slice = [].slice;
 
 
 /**
@@ -24,28 +30,6 @@ Logger = SKMObject.extend({
 	  this._prepareConsole();
 	},
 
-	_prepareConsole: function() {
-	  this._console = window.console;
-	  // if the browser does not support console(IE, mobiles, etc)
-	  if(this.consoleUnavailable())
-	    this._clearUndefinedConsole();
-	},
-
-	_clearUndefinedConsole: function() {
-	  var c = this._console || {};
-	  for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();)c[a]=c[a] || function() {};
-	  // is it safe?!
-	  this._console = c;
-	},
-
-	disablePrinter: function() {
-	  window.console = window.console || {};
-	  var c = function(){};
-	  for(var d="info,debug,error,log".split(","), a; a=d.pop();)
-	    window.console[a]=c;
-	  return true;
-	},
-
 	consoleUnavailable: function() {
 	  return typeof (window.console !== 'undefined');
 	},
@@ -54,24 +38,50 @@ Logger = SKMObject.extend({
 
 	debug: function() {
 	  if(typeof this._console.debug === 'function')
-	    this._console.debug.apply(console, [].slice.call(arguments));
+	    this._console.debug.apply(console, slice.call(arguments));
 	},
 
 	info: function() {
 	  if(typeof this._console.info === 'function')
-	    this._console.info.apply(console, [].slice.call(arguments));
+	    this._console.info.apply(console, slice.call(arguments));
 	},
 
 	warn: function() {
 	  if(typeof this._console.warn === 'function')
-	    this._console.warn.apply(console, [].slice.call(arguments));
+	    this._console.warn.apply(console, slice.call(arguments));
 	},
 
 	error: function() {
 	  if(typeof this._console.error === 'function')
-	    this._console.error.apply(console, [].slice.call(arguments));
+	    this._console.error.apply(console, slice.call(arguments));
+	},
+
+	_prepareConsole: function() {
+	  this._console = window.console;
+	  // if the browser does not support console(IE, mobiles, etc)
+	  if(this.consoleUnavailable())
+	    this._clearUndefinedConsole();
+	},
+
+	/**
+	 * Better console wrapper
+	 * @see paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
+	 */
+	_clearUndefinedConsole: function() {
+	  var c = this._console || {};
+	  for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();)c[a]=c[a] || function() {};
+	  // is it safe?!
+	  this._console = c;
 	}
 });
+
+
+// a small shortcut for console.log
+// only for development debugging!!!
+if ( window.console )
+	window.cl = console.log.debug;
+else
+	window.cl = function() {};
 
 
 return Logger;
