9506c087913db61f26161621e13e01c11d40ab34
Dragos Tudorache <hedphug@gmail.com>
2013-05-21 13:43:58 +0300

    updated require.js build config

--- requirejs-build-framework-config.js
+++ requirejs-build-framework-config.js
@@ -1,51 +1,19 @@
 ({
   baseUrl: "./js",
 
-  // appDir: "./",
-
-  // name: 'app',
-
-  // modules: [
-    // {
-      // name: "lib/almond.js"
-  //     name: 'main',
-  //     // exclude: [ "main" ]
-    // }
-  // ],
-
-  // dir: 'build',
-
-
-
-  // START Single file build
-
   out: 'build/RTFApi.build.js',
 
   // By default, if no "optimize" value is declared it will uglify
   // and minimize the build filed
   // optimize: "uglify",
-  // optimize: "none",
+  optimize: "none",
 
   include: ["skm/rtf/RTFApi"],
 
-  // END Single file build
-
-
-  preserveLicenseComments: false,
+  // preserveLicenseComments: true,
 
   paths: {
-    "lib": "lib",
-    "app": "app",
-    "views": "app/views",
-    "controllers": "app/controllers",
-    "templates": "app/templates",
-    "models": "app/models",
-
-    // "console": "/Users/dragos/Sites/SKeeM/js/lib/console-wrapper",
-    "skm": "/Users/dragos/Sites/SKeeM/js/lib/skm",
-    // "rtf": "/Users/dragos/Sites/SKeeM/js/lib/skm/rtf",
-    // "skm": "http://10.0.3.98:82/SKeeM/js/lib/skm",
-    // "rtf": "/Users/dragos/Sites/SKeeM/js/lib/skm/rtf"
+    "skm": "/Users/dragos/Sites/SKeeM/js/lib/skm"
   }
 })
 
