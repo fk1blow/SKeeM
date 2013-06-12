df21b34df2a58283ce8d02313fddd1911bb7c954
Dragos Tudorache <hedphug@gmail.com>
2013-06-12 13:54:45 +0300

    repaired the Logger, again, after a bad commit

--- js/lib/skm/util/Logger.js
+++ js/lib/skm/util/Logger.js
@@ -1,7 +1,13 @@
 
+// skm Logger module
+
+define(['skm/k/Object'],
+	function(SKMObject)
+{
+'use strict';
 
 
-define(['skm/k/Object'], function(SKMObject) {
+var slice = Array.prototype.slice;
 
 
 /**
@@ -11,41 +17,15 @@ define(['skm/k/Object'], function(SKMObject) {
  * even in browser that don't support it
  * @author Paul Irish, linked from http://www.jquery4u.com/snippets/lightweight-wrapper-firebug-console-log/#.T-2xA-HWRhE
  */
-Logger = SKMObject.extend({
-	TYPE: 'Logger',
+var Logger = function() {
+	this.TYPE = 'Logger';
+	this._instance = null;
+	this._console = null;
+	this._enabled = true;	
+	this._prepareConsole();
+}
 
-	_instance: null,
-
-	_console: null,
-
-	_enabled: true,
-
-	initialize: function(options) {
-	  this._prepareConsole();
-	},
-
-	_prepareConsole: function() {
-	  this._console = window.console;
-	  // if the browser does not support console(IE, mobiles, etc)
-	  if(this.consoleUnavailable())
-	    this._clearUndefinedConsole();
-	},
-
-	_clearUndefinedConsole: function() {
-	  var c = this._console || {};
-	  for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();)c[a]=c[a] || function() {};
-	  // is it safe?!
-	  this._console = c;
-	},
-
-	disablePrinter: function() {
-	  window.console = window.console || {};
-	  var c = function(){};
-	  for(var d="info,debug,error,log".split(","), a; a=d.pop();)
-	    window.console[a]=c;
-	  return true;
-	},
-
+Logger.prototype = {
 	consoleUnavailable: function() {
 	  return typeof (window.console !== 'undefined');
 	},
@@ -54,24 +34,50 @@ Logger = SKMObject.extend({
 
 	debug: function() {
 	  if(typeof this._console.debug === 'function')
-	    this._console.debug.apply(console, [].slice.call(arguments));
+	    this._console.debug.apply(console, slice.call(arguments));
 	},
 
 	info: function() {
-	  if(typeof this._console.info === 'function')
-	    this._console.info.apply(console, [].slice.call(arguments));
+  if(typeof this._console.info === 'function')
+	    this._console.info.apply(console, slice.call(arguments));
 	},
 
 	warn: function() {
 	  if(typeof this._console.warn === 'function')
-	    this._console.warn.apply(console, [].slice.call(arguments));
+	    this._console.warn.apply(console, slice.call(arguments));
 	},
 
 	error: function() {
 	  if(typeof this._console.error === 'function')
-	    this._console.error.apply(console, [].slice.call(arguments));
+	    this._console.error.apply(console, slice.call(arguments));
+	},
+
+	_prepareConsole: function() {
+	  this._console = window.console;
+	  // if the browser does not support console(IE, mobiles, etc)
+	  if ( this.consoleUnavailable() )
+	    this._clearUndefinedConsole();
+	},
+
+	/**
+	 * Better console wrapper
+	 * @see paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
+	 */
+	_clearUndefinedConsole: function() {
+	  var c = this._console || {};
+	  for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();)c[a]=c[a] || function() {};
+	  // is it safe?!
+	  this._console = c;
 	}
-});
+};
+
+
+// a small shortcut for console.log
+// only for development debugging!!!
+if ( window.console )
+	window.cl = console.log;
+else
+	window.cl = function() {};
 
 
 return Logger;
