efbdfc496f8e14b0219eeca3f6fda22633b64f2a
Dragos Tudorache <hedphug@gmail.com>
2013-07-10 19:08:03 +0300

    added some java logging references and resources

--- js/lib/skm/util/NewLogger.js
+++ js/lib/skm/util/NewLogger.js
@@ -1,4 +1,30 @@
 
+/**
+ * @see  http://www.crazysquirrel.com/computing/java/logging.jspx
+ *
+ * 
+ * @see  http://www.javapractices.com/topic/TopicAction.do?Id=143
+ * 
+ * <code>
+ *	logging request of some level is made to logger attached to current package 
+ *	if the request level is too low for that package's logger { 
+ *	   discard it 
+ *	} 
+ *		otherwise { 
+ *		  cycle through all handlers { 
+ *		    if the request level is too low for that handler { 
+ *		      discard it 
+ *		    } 
+ *		    otherwise { 
+ *		      log the request 
+ *		    }
+ *		  }
+ *		}
+ *	</code>
+
+ */
+
+
 // skm Logger module
 
 define(['/skm/k/Objekt'], function(SKMObject) {
