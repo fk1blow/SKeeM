25d5b2f26b6f31cf7764966946f589f8d28f59c3
Dragos Tudorache <hedphug@gmail.com>
2013-07-12 12:36:12 +0300

    added removeHandler, normalized Config, Logger options and default handler

--- js/lib/skm/util/NewLogger.js
+++ js/lib/skm/util/NewLogger.js
@@ -88,7 +88,11 @@ var Loggable = {
  * @param  {Object} handler the handler assigned to this logger or
  * get default logger if not available
  */
-var Logger = function(name) {
+var Logger = function(name, options) {
+	this.options = options || {};
+
+// cl(arguments)
+
 	/**
 	 * The name identifier ot this Logger instance
 	 * @type {String}
@@ -106,19 +110,12 @@ var Logger = function(name) {
 	 * or the default one - [Level.ALL]
 	 * @type {Level}
 	 */
-	this._level = Config.DefaultLevel;
-
-
-	/**
-	 * Default object that has a list of handlers used for by default
-	 * @type {ConsoleHandler}
-	 */
-	this._defaultHandlerObject = Config.DefaultHandler;
+	this._level = this.getDefaultLevel();
 
 
 	// Adds the default handler available on default handler object
 	// Default handler is the ConsoleHandler - console in a browser env
-	this._addDefaultHandlersList();
+	this._addDefaultHandlersList(options.handlersList);
 };
 
 
@@ -128,7 +125,7 @@ SKMObject.mixin(Logger.prototype, Loggable, {
 	 * @return {Level} 
 	 */
 	getDefaultLevel: function() {
-		return Config.DefaultLevel;
+		return this.options.defaultLevel || 0;
 	},
 
 	/**
@@ -142,12 +139,13 @@ SKMObject.mixin(Logger.prototype, Loggable, {
 	},
 
 	/**
-	 * TBD
-	 * @param  {[type]} handler [description]
-	 * @return {[type]}         [description]
+	 * Removes a handler from the handlers list
+	 * @param  {Function} handler A recerence to a handler function in the list
 	 */
 	removeHandler: function(handler) {
-		//
+		var index = this._handlers.indexOf(handler);
+		if ( index >= 0 )
+			this._handlers.splice(index, 1);
 	},
 
 	/**
@@ -175,22 +173,33 @@ SKMObject.mixin(Logger.prototype, Loggable, {
 	isLoggable: function(level) {
 		var currentLevel = this.getLevel();
 		level = (typeof level !== 'number') ? currentLevel : level;
+		
+		// level is -1, not loggable
+		if ( this.isDisabled() )
+			return false;
+
 		// If the required level is 0, it means all levels are loggable
-		if ( level === Level.ALL )
+		// or if the current level is smaller or eq to required, logger is loggable
+		if ( level === Level.ALL || currentLevel <= level )
 			return true;
-		// if the current level is smaller or eq to required, logger is loggable
-		if ( this.getLevel() <= level )
-			return true;
+	},
+
+	/**
+	 * Test if the current level is Level.OFF == "-1"
+	 * @return {Bool}
+	 */
+	isDisabled: function() {
+		return this.getLevel() === Level.OFF;
 	},
 
 	/*
 		Privates
 	 */
 
-	_addDefaultHandlersList: function() {
-		var handler = null, handlerObject = this._defaultHandlerObject;
-		for ( handler in handlerObject )
-			this.addHandler(handlerObject[handler]);
+	_addDefaultHandlersList: function(handlersList) {
+		var handler = null;
+		for ( handler in handlersList )
+			this.addHandler(handlersList[handler]);
 	},
 
 	_processHandlers: function(message) {
@@ -214,28 +223,25 @@ SKMObject.mixin(Logger.prototype, Loggable, {
  * for a Logger object. It should adhere to a Loggable interface
  * that will be used for every handler.
  */
-/*var ConsoleHandler = function() {
-	// check if console available
-	// use those checks available inside /utile/Logger.js
-}*/
-
-
-// ConsoleHandler.prototype = {
-var ConsoleHandler = {
+var ConsoleHandlers = {
 	debug: function() {
-		console.log.apply(this, arguments);
+		if ( console && typeof console.debug === 'function' )
+			console.log.apply(console, arguments);
 	},
 
 	info: function() {
-		console.info.apply(this, arguments);
+		if ( console && typeof console.info === 'function' )
+			console.info.apply(console, arguments);
 	},
 
 	warn: function() {
-		console.warn.apply(this, arguments);
+		if ( console && typeof console.warn === 'function' )
+			console.warn.apply(console, arguments);
 	},
 	
 	error: function() {
-		console.error.apply(this, arguments);
+		if ( console && typeof console.error === 'function' )
+			console.error.apply(console, arguments);
 	}
 };
 
@@ -247,14 +253,16 @@ var ConsoleHandler = {
 var Level = {
 	OFF: -1,
 	ALL: 0,
+
 	FINEST: 1,
 	FINER: 2,
 	FINE: 3,
 	CONFIG: 4,
 	INFO: 5,
 	WARNING: 6,
-	SEVERE: 7
-}
+	SEVERE: 7,
+	ERROR: 8
+};
 
 
 /*------------------------------------
@@ -265,29 +273,44 @@ var Level = {
 var Config = {
 	DefaultLevel: Level.ALL,
 
-	DefaultHandler: ConsoleHandler
+	DefaultHandlers: ConsoleHandlers
 };
 
 
-var LoggerManager = {
-	_list: {},
+var LoggerManager = (function(){
+	var loggerList = {};
 
-	addLogger: function(name, logger) {
-		this._list[name] = logger;
-	},
+	var getLoggerOptions = function() {
+		return {
+			defaultLevel: Config.DefaultLevel,
+			handlersList: Config.DefaultHandlers
+		}
+	};
 
-	getLogger: function(name) {
-		return this._list[name] || new Logger(name);
+	return {
+		createLogger: function(name) {
+			var options = getLoggerOptions();
+			return loggerList[name] = new Logger(name, options);
+		},
+
+		getLogger: function(name) {
+			return loggerList[name] || this.createLogger(name);
+		},
+
+		turnOffLoggers: function() {
+			for ( var logger in loggerList )
+				loggerList[logger].setLevel(Level.OFF);
+		}
 	}
-}
+}());
 
 
 return {
 	Manager: LoggerManager,
 
-	Level: Level
+	Level: Level,
 
-	// ConsoleHandler: ConsoleHandler
+	Config: Config
 };
 
 
