e96b10c0d2bbec708af1972a492c015e3abc6cc8
Dragos Tudorache <hedphug@gmail.com>
2013-10-25 15:44:56 +0300

    cleaned, refactored and removed some bugs
    removed continuous tick bug
    removed the ambiguity related to the getTick() method
    added the posibility to attach a callback for the tick - until now, the Timer should have container an Event mixin...

--- js/lib/skm/util/Timer.js
+++ js/lib/skm/util/Timer.js
@@ -1,4 +1,3 @@
-
 // Simple Timer object
 
 define(['skm/util/Subscribable',
@@ -16,16 +15,6 @@ define(['skm/util/Subscribable',
  * @link http://closure-library.googlecode.com/svn/docs/closure_goog_timer_timer.js.html
  */
 var Timer = SKMObject.extend(Subscribable, {
-  // How many times the interval will
-  // trigger a tick; (x < 1) == infinity
-  ticks: 1,
-
-  // The interval at which a tick is being triggered
-  tickInterval: 1000,
-
-  // If the timer is active or not
-  enabled: false,
-
   _intervalScale: 0.8,
 
   // Timer object reference
@@ -37,10 +26,13 @@ var Timer = SKMObject.extend(Subscribable, {
   _lastTickTime: 0,
 
   initialize: function(options) {
-    options || (options = {});
+    var _base, _ref;
+    this.options = options;
+    if ((_ref = (_base = this.options).tickInterval) == null) {
+      _base.tickInterval = 1000;
+    }
+    this.options.ticks = this.options.ticks === 0 ? 0 : this.options.ticks || 0;
     this._timerObject = null;
-    this.ticks = (options.ticks === 0) ? 0  : options.ticks;
-    this.tickInterval = options.tickInterval || 1000;
   },
 
   /**
@@ -48,55 +40,40 @@ var Timer = SKMObject.extend(Subscribable, {
    */
 
   start: function() {
-    var that = this;
+    var that;
+    that = this;
     this.enabled = true;
-    // Start only if the timerObject is not assigned(or null)
-    if ( !this._timerObject ) {
+    if (!this._timerObject) {
       this._tickCounter = 0;
       this._timerObject = setTimeout(function() {
-        that._tickTack();
-      }, this.tickInterval);
+        return that._tickTack();
+      }, this.options.tickInterval);
       this._lastTickTime = this.now();
     }
     return this;
   },
 
   stop: function() {
-    var lastTickCounter = this.getTicks();
     this.enabled = false;
     this._tickCounter = 0;
-    if ( this._timerObject ) {
+    if (this._timerObject) {
       clearTimeout(this._timerObject);
       this._timerObject = null;
     }
     return this;
   },
 
-  /**
-   * Handlers
-   */
-
   handleTick: function(ticks) {
-    this.fire('tick', ticks);
+    var _ref;
+    return (_ref = this.options.tickHandler) != null ? _ref.call(this, ticks) : void 0;
   },
-
-  /**
-   * Getters/Setters
-   */
-
-  getTicks: function() {
-    if ( this.ticks < 1 )
-      return 0;
-    else
-      return this.ticks;
-  },
-
 
   maxTicksReached: function() {
-    if ( this.getTicks() === 0 )
+    if (this.options.ticks === 0) {
       return false;
-    else
-      return this._tickCounter >= this.getTicks();
+    } else {
+      return this._tickCounter >= this.options.ticks;
+    }
   },
 
   now: function() {
@@ -108,37 +85,29 @@ var Timer = SKMObject.extend(Subscribable, {
    */
 
   _tickTack: function() {
-    if ( this.enabled ) {
-      var that = this, elapsed, notSynced;
-
-      // Stop if reached maximum ticks set
-      if ( this.maxTicksReached() ) {
+    var elapsed, notSynced, that;
+    if (this.enabled) {
+      that = this;
+      elapsed = void 0;
+      notSynced = void 0;
+      if (this.maxTicksReached()) {
         this.stop();
         return;
       }
-
-      // Synchronize the interval with the elapsed time
-      // @see closure-library.googlecode.com/svn/docs/closure_goog_timer_timer.js.html
       elapsed = this.now() - this._lastTickTime;
-      notSynced = elapsed > 0 && elapsed < (this.tickInterval * this._intervalScale);
-      if ( notSynced ) {
+      notSynced = elapsed > 0 && elapsed < (this.options.tickInterval * this._intervalScale);
+      if (notSynced) {
         this._timerObject = setTimeout(function() {
-          that._tickTack();
-        }, this.tickInterval - elapsed);
+          return that._tickTack();
+        }, this.options.tickInterval - elapsed);
         return;
       }
-      
-      // Handle the ticks and increment internal counter
-      this.handleTick.call(this, this.getTicks());
-      this._tickCounter++;
-      
-      // In goog.timer, this re-check is required becase a timer may be
-      // stopped between a tick so that [this.enabled] could be reset
-      if ( this.enabled ) {
+      this.handleTick.call(this, ++this._tickCounter);
+      if (this.enabled) {
         this._timerObject = setTimeout(function() {
-          that._tickTack();
-        }, this.tickInterval);
-        this._lastTickTime = this.now();
+          return that._tickTack();
+        }, this.options.tickInterval);
+        return this._lastTickTime = this.now();
       }
     }
   }
